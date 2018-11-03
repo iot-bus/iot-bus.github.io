6154b46590cf5f4b4da23d714e30cbb5a9179946
ianarchbell <ian@Ians-iMac-9.local>
2018-11-03 09:37:20 -0700

    Added setup to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -87,6 +87,10 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
 
+def setup(app):
+    app.add_stylesheet('css/extra.css')
+
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
